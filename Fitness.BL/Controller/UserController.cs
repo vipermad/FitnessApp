d8789f7a075bde289b097f8c32d9369b4dd29c12
vipermad <vipermad91@gmail.com>
2020-05-02 17:45:45 +0300

    Создано модель пользователя.

---
+++ Fitness.BL/Controller/UserController.cs
@@ -0,0 +1,19 @@
+﻿using Fitness.BL.Model;
+using System;
+
+
+namespace Fitness.BL.Controller
+{
+    public class UserController
+    {
+        public User User { get; }
+        public UserController(User user)
+        {
+            User = user ?? throw new ArgumentNullException("Пользователь не может быть равен null", nameof(user));
+        }
+        public bool Save()
+        {
+
+        }
+    }
+}
